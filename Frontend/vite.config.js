73dc4cbd3c68e10db819dec80036356a9f711c29
Khushal Patil <khushal3114@gmail.com>
2025-05-22 00:39:55 +0530

    fix: added vite and build script

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import flowbiteReact from "flowbite-react/plugin/vite";
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [tailwindcss(), flowbiteReact()],
+  plugins: [tailwindcss(), flowbiteReact(), react()],
 })
